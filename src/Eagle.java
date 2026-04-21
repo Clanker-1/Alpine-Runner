f6cce5d472e7a7e0107ebb503a82ab142dbaea03
Quinn Dawson <9671457@graniteschools.org>
2026-04-21 11:37:42 -0600

    Update Eagle.java

--- src/Eagle.java
+++ src/Eagle.java
@@ -1,1 +1,27 @@
+public class Eagle {
 
+    private float positionX;
+    private float positionY;
+
+    public Eagle(float startX, float startY) {
+        this.positionX = startX;
+        this.positionY = startY;
+    }
+
+    public void update() {
+        positionX -= 1.0f;
+
+        if (positionX < -5) {
+            positionX = 40;
+            positionY = 3 + (float)(Math.random() * 5);
+        }
+    }
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public float getPositionY() {
+        return positionY;
+    }
+}
