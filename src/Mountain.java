2115c491c7f417c75b387c9bdab2269c5a6a8082
Quinn Dawson <9671457@graniteschools.org>
2026-04-21 11:37:24 -0600

    Add Mountain class with position and update logic

--- src/Mountain.java
+++ src/Mountain.java
@@ -1,1 +1,26 @@
+public class Mountain {
 
+    private float positionX;
+    private float width;
+
+    public Mountain(float startX, float width) {
+        this.positionX = startX;
+        this.width = width;
+    }
+
+    public void update() {
+        positionX -= 0.7f;
+
+        if (positionX < -5) {
+            positionX = 30;
+        }
+    }
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+}
