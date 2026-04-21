0735b18680ad57d07cc55e43d3a5fe49531f6743
Quinn Dawson <9671457@graniteschools.org>
2026-04-21 11:35:42 -0600

    Update Render.java

--- src/Render.java
+++ src/Render.java
@@ -1,1 +1,18 @@
+public class Render {
 
+    public void renderGoat(Goat goat) {
+        System.out.println("Goat at (" + goat.getPositionX() + ", " + goat.getPositionY() + ")");
+    }
+
+    public void renderMountain(Mountain mountain) {
+        System.out.println("Mountain at x=" + mountain.getPositionX());
+    }
+
+    public void renderEagle(Eagle eagle) {
+        System.out.println("Eagle at (" + eagle.getPositionX() + ", " + eagle.getPositionY() + ")");
+    }
+
+    public void renderScore(int score) {
+        System.out.println("Score: " + score);
+    }
+}
