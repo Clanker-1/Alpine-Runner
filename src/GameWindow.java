ca637cd24331b2f4b63b03a80bdde0ea5afe42ce
Quinn Dawson <9671457@graniteschools.org>
2026-04-21 12:04:50 -0600

    Implement KeyListener for GameWindow class

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GameWindow extends JPanel {
+public class GameWindow extends JPanel implements KeyListener {
 
     private Game game;
 
@@ -12,6 +13,7 @@ public class GameWindow extends JPanel {
         frame.setSize(800, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
+        frame.addKeyListener(this);
         frame.setVisible(true);
     }
 
@@ -19,32 +21,38 @@ public class GameWindow extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Background
+        // sky
         g.setColor(new Color(180, 220, 255));
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        // Ground
+        // ground
         g.setColor(new Color(100, 200, 100));
         g.fillRect(0, 300, getWidth(), 100);
 
-        // Goat
+        // goat
         g.setColor(Color.BLACK);
-        g.fillRect((int) game.goat.getPositionX(), 
-                   300 - (int) game.goat.getPositionY(), 
-                   20, 20);
+        g.fillRect(game.goat.x, 300 - game.goat.y - 40, 40, 40);
 
-        // Mountain
+        // mountain
         g.setColor(Color.DARK_GRAY);
-        g.fillRect((int) game.mountain.getPositionX(), 260, 40, 40);
+        g.fillRect(game.mountain.x, 260, 40, 40);
 
-        // Eagle
+        // eagle
         g.setColor(Color.RED);
-        g.fillOval((int) game.eagle.getPositionX(), 
-                   200 - (int) game.eagle.getPositionY(), 
-                   20, 20);
+        g.fillOval(game.eagle.x, 200 - game.eagle.y, 30, 30);
 
-        // Score
+        // score
         g.setColor(Color.BLACK);
         g.drawString("Score: " + game.score, 20, 20);
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            game.goat.jump();
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {}
 }
