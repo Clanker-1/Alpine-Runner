6fa59d282e09f4fce286416133cae2cf75ec4f64
Quinn Dawson <9671457@graniteschools.org>
2026-05-02 17:57:31 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mountain Runner");
+        Game game = new Game();
+
+        frame.add(game);
+        frame.setSize(800, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        game.start();
+    }
+}
