9b28f5faed0a62d597c1c554770cbc95a2e6f5c2
Quinn Dawson <9671457@graniteschools.org>
2026-05-03 18:22:36 -0600

    Modify Mountain class to use speed parameter

--- src/Mountain.java
+++ src/Mountain.java
@@ -3,25 +3,27 @@ import java.awt.*;
 public class Mountain {
 
     float positionX;
+    int speed;
 
-    public Mountain(float startX) {
+    public Mountain(float startX, int speed) {
         this.positionX = startX;
+        this.speed = speed;
     }
 
     public void update() {
-        positionX -= 5;
+        positionX -= speed;
     }
 
     public void draw(Graphics g) {
         g.setColor(Color.GRAY);
 
         int[] xPoints = {(int) positionX, (int) positionX + 20, (int) positionX + 40};
-        int[] yPoints = {350, 300, 350};
+        int[] yPoints = {330, 280, 330};
 
         g.fillPolygon(xPoints, yPoints, 3);
     }
 
     public Rectangle getBounds() {
-        return new Rectangle((int) positionX, 300, 40, 50);
+        return new Rectangle((int) positionX, 280, 40, 50);
     }
 }
