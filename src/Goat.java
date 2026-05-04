9ebc2669034fd1655cf604b3fd1a9df85611915a
Quinn Dawson <9671457@graniteschools.org>
2026-05-03 18:50:24 -0600

    Refactor animation variables in Goat class

--- src/Goat.java
+++ src/Goat.java
@@ -10,8 +10,8 @@ public class Goat {
 
     boolean isJumping = false;
 
-    int animationFrame = 0;
-    int animationCounter = 0;
+    int frame = 0;
+    int counter = 0;
 
     public void jump() {
         if (!isJumping) {
@@ -30,38 +30,33 @@ public class Goat {
             isJumping = false;
         }
 
-        // animation
-        animationCounter++;
-        if (animationCounter > 10) {
-            animationFrame = (animationFrame + 1) % 2;
-            animationCounter = 0;
+        counter++;
+        if (counter > 10) {
+            frame = (frame + 1) % 2;
+            counter = 0;
         }
     }
 
     public void draw(Graphics g) {
         g.setColor(Color.WHITE);
 
-        // body
         g.fillRect((int) positionX, (int) positionY, 30, 20);
-
-        // head
         g.fillOval((int) positionX + 20, (int) positionY - 10, 15, 15);
 
         g.setColor(Color.BLACK);
 
-        // animated legs
-        if (animationFrame == 0) {
+        if (frame == 0) {
             g.drawLine((int) positionX + 5, (int) positionY + 20,
-                       (int) positionX + 5, (int) positionY + 30);
+                    (int) positionX + 5, (int) positionY + 30);
 
             g.drawLine((int) positionX + 20, (int) positionY + 20,
-                       (int) positionX + 20, (int) positionY + 30);
+                    (int) positionX + 20, (int) positionY + 30);
         } else {
             g.drawLine((int) positionX + 5, (int) positionY + 20,
-                       (int) positionX + 10, (int) positionY + 30);
+                    (int) positionX + 10, (int) positionY + 30);
 
             g.drawLine((int) positionX + 20, (int) positionY + 20,
-                       (int) positionX + 15, (int) positionY + 30);
+                    (int) positionX + 15, (int) positionY + 30);
         }
     }
 
